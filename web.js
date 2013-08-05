52cc7fa1d2c940d52f519ed6307c1a13614291c6
ASHISH MISHRA <sendtoashishmishra@outlook.com>
2013-08-05 23:21:15 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,21 +1,18 @@
-var express = require('express');
-var app = express();
-var fs= require('fs');
-app.use(express.logger());
+var express = require('express')
+  , fs = require('fs');
 
+var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/assets'));
 
-
-app.get('/', function(request, response) {
-	
-	var  myhtml = fs.readFileSync('mywebsite/index.html','utf8');
-        
-
-        response.send(myhtml);
+app.get('/', function (req, res) {
+  // async is always better
+  fs.readFile('index.html', function (err, text) {
+    res.setHeader('Content-Type', 'text/html');
+    res.end(text);
+  });
 });
-
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+  console.log("Listening on %s...", port);
 });
-
